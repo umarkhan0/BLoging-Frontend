3019f6a982d21e0da6e9af1009c83f5438d5fd69
Umar khan <umaraamir959@gmail.com>
2024-09-20 14:10:08 +0500

    Update

--- app/constant/const.js
+++ app/constant/const.js
@@ -3,3 +3,12 @@ import axios from 'axios';
 export const apiService = axios.create({
   baseURL: 'https://bloging-server.vercel.app/api/',
 });
+let accessToken = localStorage.getItem("Sign") || ""
+  const headers = {
+  'Authorization': `Bearer ${accessToken}`,
+  'Content-Type': 'application/json',
+};
+
+export const options = {
+  headers: headers,
+};
