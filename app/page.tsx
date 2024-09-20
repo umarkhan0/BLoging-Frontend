cd058d9bc4f8e5ffd132214bee1d88e5f5e2995e
Umar khan <umaraamir959@gmail.com>
2024-09-20 19:12:30 +0500

    Remove extra files

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,9 @@ import Blog from "@/components/Blog";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
-import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -18,15 +16,12 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  console.log("hello");
   
   return (
     <>
 
       <ScrollUp />
       <Hero />
-      {/* <Features /> */}
-      {/* <Video /> */}
       {/* <Brands /> */}
       {/* <AboutSectionOne /> */}
       <AboutSectionTwo />
