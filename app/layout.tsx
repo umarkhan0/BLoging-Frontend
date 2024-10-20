e62988b6d40aca61c11d6dec8923c81763c1aa6f
Umar khan <umaraamir959@gmail.com>
2024-10-20 20:33:54 +0500

    Add Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
     <html suppressHydrationWarning lang="en">
       <meta name="monetag" content="71dd813c1ac21dbb26b22c1eed374731"></meta>
       <script src="https://alwingulla.com/88/tag.min.js" data-zone="109236" async data-cfasync="false"></script>
+      <script  data-cfasync="false" src="//thubanoa.com/1?z=8277756"></script>
+
+      <script  data-cfasync="false" src="//thubanoa.com/1?z=8277789"></script>
+      
       <head />
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
